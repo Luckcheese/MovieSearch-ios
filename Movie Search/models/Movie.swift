8ba0d7fd38c5e1aaceab3362398f9938062b69d7
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2016-02-28 23:16:13 -0300

    Added Movie class: will be the movie with all its info

---
+++ Movie Search/models/Movie.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+class Movie {
+
+    var title: String?
+    var year: String?
+    var rated: String?
+    var released: String?
+    var runtime: String?
+    var genre: String?
+    var director: String?
+    var writer: String?
+    var actors: String?
+    var plot: String?
+    var language: String?
+    var country: String?
+    var awards: String?
+    var poster: String?
+    var metascore: String?
+    var imdbRating: String?
+    var imdbVotes: String?
+    var imdbID: String?
+    var type: String?
+
+    init() {
+        title = "Movie Title"
+        year = "2000"
+        rated = "90"
+        released = "10/10/2000"
+        runtime = "100"
+        genre = "legal"
+        director = "alguem"
+        writer = "alguem"
+        actors = "umas pessoas"
+        plot = "a volta dos que não foram"
+        language = "falada"
+        country = "HUEbr"
+        awards =  "nenhum"
+        metascore = "90"
+    }
+}
