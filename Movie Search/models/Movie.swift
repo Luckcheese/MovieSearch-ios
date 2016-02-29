63d3cac55ccecf4eb4f92e1f5770982aed224269
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2016-02-29 08:59:01 -0300

    Minor refactoring on Movie class: made all variables automatically unwrapped

--- Movie Search/models/Movie.swift
+++ Movie Search/models/Movie.swift
@@ -2,25 +2,25 @@ import Foundation
 
 class Movie {
 
-    var title: String?
-    var year: String?
-    var rated: String?
-    var released: String?
-    var runtime: String?
-    var genre: String?
-    var director: String?
-    var writer: String?
-    var actors: String?
-    var plot: String?
-    var language: String?
-    var country: String?
-    var awards: String?
-    var poster: String?
-    var metascore: String?
-    var imdbRating: String?
-    var imdbVotes: String?
-    var imdbID: String?
-    var type: String?
+    var title: String!
+    var year: String!
+    var rated: String!
+    var released: String!
+    var runtime: String!
+    var genre: String!
+    var director: String!
+    var writer: String!
+    var actors: String!
+    var plot: String!
+    var language: String!
+    var country: String!
+    var awards: String!
+    var poster: String!
+    var metascore: String!
+    var imdbRating: String!
+    var imdbVotes: String!
+    var imdbID: String!
+    var type: String!
 
     init() {
         title = "Movie Title"
@@ -32,7 +32,7 @@ class Movie {
         director = "alguem"
         writer = "alguem"
         actors = "umas pessoas"
-        plot = "a volta dos que não foram"
+        plot = "a volta dos que não foram a volta dos que não foram a volta dos que não foram a volta dos que não foram a volta dos que não foram a volta dos que não foram a volta dos que não foram a volta dos que não foram"
         language = "falada"
         country = "HUEbr"
         awards =  "nenhum"
